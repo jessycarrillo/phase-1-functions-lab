1a40b0e2077456b2dad2444b7a4f6fa2cd44703d
Jessica Carrillo <jessymamo@gmail.com>
2023-06-29 16:23:32 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const manhattanFeetPerBlock = 264
+const hqStAddress = 42
+
+
+function distanceFromHqInBlocks(passengerStLocation){
+    return Math.abs(passengerStLocation - hqStAddress); 
+}  
+
+function distanceFromHqInFeet(passengerStLocation) {
+    const blocks = distanceFromHqInBlocks(passengerStLocation);
+    return blocks * manhattanFeetPerBlock;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock){
+    const traveledBlock = endBlock - startBlock
+    return Math.abs(traveledBlock * manhattanFeetPerBlock)
+}
+
+function calculatesFarePrice(startBlock, endBlock){
+    const ride =  Math.abs((endBlock - startBlock) * manhattanFeetPerBlock)
+    if (ride <= 400) {
+        return 0
+        }
+      if((ride > 400) && (ride < 2000)) {
+        return (ride - 400) * 0.02
+    }
+    if ((ride >= 2000) && (ride < 2500)){
+        return 25;
+      }
+    if (ride >= 2500)
+      return 'cannot travel that far';
+  }
+
+
